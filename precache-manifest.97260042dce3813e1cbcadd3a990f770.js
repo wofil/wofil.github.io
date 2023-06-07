cb508b4fd1b1ddde8f1e8a7d5be28847df228a67
Michael Pulis <mikeypulis@gmail.com>
2023-06-07 22:28:07 +0200

    hello

---
+++ precache-manifest.97260042dce3813e1cbcadd3a990f770.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "88c6eee8a77bce52dfee",
+    "url": "/static/css/main.4bc61ff3.chunk.css"
+  },
+  {
+    "revision": "88c6eee8a77bce52dfee",
+    "url": "/static/js/main.88c6eee8.chunk.js"
+  },
+  {
+    "revision": "5df50f20135844d1f62b",
+    "url": "/static/js/1.5df50f20.chunk.js"
+  },
+  {
+    "revision": "2a14f2065c06b7ebe4c6",
+    "url": "/static/css/2.abbd0627.chunk.css"
+  },
+  {
+    "revision": "2a14f2065c06b7ebe4c6",
+    "url": "/static/js/2.2a14f206.chunk.js"
+  },
+  {
+    "revision": "7783f96e75bead0d1762",
+    "url": "/static/js/runtime~main.7783f96e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "58ee7fbab62eb0b12365becace9d4075",
+    "url": "/index.html"
+  }
+];
